c1c09f72ad0ee6a3c6424d481b6de03d7e8aaf0b
Ahmed Gaafar <96004565+AhmedJ3far@users.noreply.github.com>
2023-03-14 18:48:44 +0200

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,19 @@
+
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let seven = document.querySelector(".seven");
+
+console.log(window.scrollY);
+console.log(one.offsetTop);
+console.log(two.offsetTop);
+console.log(three.offsetTop);
+console.log(four.offsetTop);
+console.log(five.offsetTop);
+
+// make it responsive 
+// add functionality and animations 
+// complete the full look
